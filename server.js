292728c42ca9471d39603937d14ec235225bce1e
Brian Watroba <brianwatroba@gmail.com>
2021-11-04 10:40:46 -0400

    first commit. Express server set up, serves static HTML from ./public, currently displays hello world boilerplate. Initial dev and express dependencies set up

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log('Server listening on http://localhost:5000');
+});
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
